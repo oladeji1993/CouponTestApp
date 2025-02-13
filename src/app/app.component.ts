afd706d362f49482d907502e366e09c4a6cf5ccd
oladeji.revent <oladeji.revent@zenithbank.com>
2025-02-13 12:24:51 +0100

    updated css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PipesModule } from './Core/pipes/pipes.module';
 
@@ -10,8 +10,19 @@ import { PipesModule } from './Core/pipes/pipes.module';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
 
-  
+  ngOnInit(): void {
+    this.loadStyles('styles.scss');
+    this.loadStyles('output.css');
+    this.loadStyles('./output.css');
+  }
+
+  loadStyles(cssFilePath: string): void {
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = cssFilePath;
+    document.head.appendChild(linkElement);
+  }
   
 }
